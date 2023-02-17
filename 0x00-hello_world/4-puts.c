839850584ed1560c63786a461cef928ec00d87ac
Ayomzy96 <abrahamayomide117@gmail.com>
2023-02-17 07:14:08 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - print strings using puts command
+ *
+ * Return- Always 0
+ */
+
+int main(void)
+{
+	printf("\"programming is like building a multilingual puzzle");
+	return(0);
+}
