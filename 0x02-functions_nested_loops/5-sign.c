39db490843240f5c52d9498201ff3f1aab73e447
Ayomzy96 <abrahamayomide117@gmail.com>
2023-02-21 13:51:02 -0800

    j

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,29 @@
 #include "main.h"
 /**
-*_isalpha - controls if a character is alphabetical
-*@c: character to be verified
-*Return: return 0 or 1
+* print_sign - Determines if the input number
+* greater, iqual or less than zero.
+*
+* @n: The input number as an integer.
+*
+* Return: 1 is greater than zero. 0 is zero.
+* -1 is less than zero.
 */
-int _isalpha(int c)
+int print_sign(int n)
 {
-if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-return (1);
-
-return (0);
+	if (n > 0)
+		{
+		_putchar(43);
+	return (1);
+		}
+	else if (n < 0)
+		{
+		_putchar(45);
+	return (-1);
+		}
+	else
+		{
+		_putchar(48);
+	return (0);
+		}
+		_putchar('\n');
 }
